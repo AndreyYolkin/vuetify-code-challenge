64838f8081bb97b65bc851fec7be3b9450241b68
Andrei Elkin <andreyyolkin@gmail.com>
2025-03-06 01:09:30 +0100

    refactor: move router plugin into plugins folder

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,13 +1,6 @@
-/**
- * plugins/index.ts
- *
- * Automatically included in `./src/main.ts`
- */
-
-// Types
 import type { App } from 'vue'
-import router from '../router'
 import pinia from './pinia'
+import router from './router'
 
 // Plugins
 import vuetify from './vuetify'
